0cca1d0f8c89244981a30c22928271880153b171
srecupe2 <89435566+srecupe2@users.noreply.github.com>
2021-12-17 03:20:54 -0500

    Add files via upload
    fixed formatting for main py file, and removed the extra pynb comments, added author lines, linked back to hyppo, reworded k sample  test doc string as you requested, added references, added notes on how ksample used in multivariate feature selectaion, fixed tabbing, fixed space with Notes in Ksample, fixed parentheses, used joblib for parallel operation, wrote new nested function rather than nested class(no doc string followed from other selector class examples), fixed error messages and conditionals for error messages, concatenated features and scores,

--- sklearn/feature_selection/_multivariate_selection.py
+++ sklearn/feature_selection/_multivariate_selection.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[15]:
+# In[118]:
 
 
 from sklearn.base import BaseEstimator
@@ -9,20 +9,23 @@ from sklearn.feature_selection import SelectorMixin
 import numpy as np
 from scipy.stats import multiscale_graphcorr
 from scipy.sparse import isspmatrix
-from scipy._lib._util import MapWrapper
 import warnings
 from sklearn.utils.validation import check_is_fitted
+from joblib import Parallel, delayed
 
 ######################################################################
 # Scoring function
 
+# The following is a rewriting of hyppo.ksample.KSample
+# from hyppo.neurodata.io
 def k_sample_test(X, y,score_func="mgc"):
     """Nonparametric `K`-Sample Testing test statistic.
      
-     A k_sample test tests equality in distribution among groups. Groups
+    A k-sample test tests equality in distribution among groups. Groups
     can be of different sizes, but must have the same dimensionality.
-    There are not many non-parametric k_sample tests, and this version
-    leverages the only k_sample multivariate independence test in scipy.
+    This implementation reduces the k-sample testing to an 
+    independence testing problem, and leverages notable and powerful
+    multivariate independence tests.
     
     Read more in the :ref:`User Guide <multivariate_feature_selection>`.
     
@@ -32,25 +35,36 @@ def k_sample_test(X, y,score_func="mgc"):
         Sample vectors.
     y : ndarray of shape (n_samples,)
         The target vector.
-    score_func : string that refers to a k_sample multivariate independence test from scipy
-                The default and only existing test is multiscale graph correlation.
+    score_func : string that refers to a multivariate independence test from scipy
+        The default and only existing test is multiscale graph correlation.
     
     Returns
     -------
-    stat : float that refers to the computed k_sample test statistic
+    stat : float that refers to the computed k-sample test statistic
     
     Notes
     -----
-    
-    1) The k_sample testing problem can be thought of as a generalization of
+    1. The k-sample testing problem can be thought of as a generalization of
     the two sample testing problem. 
     
-    2) By manipulating the inputs of the k_sample test, we create
+    2. By manipulating the inputs of the k-sample test, we create
     concatenated versions of the inputs and a label matrix which are
     paired. Then, any multivariate nonparametric test can be performed on
     this data.
+    
+    3. Multivariate feature selection uses k-sample test score function to
+    calculate a test statistic for each feature not already selected as a 
+    best feature. For each feature in that sub-section, inputted is a data matrix 
+    with best features selected and that additional feature.
+    
+    References
+    ----------
+    .. [1] Sambit Panda, Cencheng Shen, Ronan Perry, Jelle Zorn, Antoine Lutz, 
+           Carey E. Priebe, and Joshua T. Vogelstein. Nonpar MANOVA via 
+           Independence Testing. arXiv:1910.08883 [cs, stat], April 2021. 
+
     """
-    #extract data matrix of shape (_samples,_features) for each group
+    # extract data matrix of shape (_samples,_features) for each group
     k_array = np.unique(y)
     matrices = []
     for i in k_array:
@@ -61,7 +75,7 @@ def k_sample_test(X, y,score_func="mgc"):
             xi = X[indices,:]
         matrices.append(xi)
     X = np.concatenate(matrices)
-    #one hot encode y for multivariate independence test
+    # one hot encode y for multivariate independence test
     vs = []
     for i in range(len(np.unique(y))):
         n = matrices[i].shape[0]
@@ -70,17 +84,17 @@ def k_sample_test(X, y,score_func="mgc"):
         vs.append(encode)
     y = np.concatenate(vs)
     
-    #mgc case
+    # mgc case
     if score_func == "mgc":
         warnings.filterwarnings("ignore")
         mgc = multiscale_graphcorr(X,y,reps = 0)
         stat = mgc.stat 
-    #default
+    # default
     else:
         warnings.filterwarnings("ignore")
         mgc = multiscale_graphcorr(X,y,reps = 0)
         stat = mgc.stat 
-    return(stat)
+    return stat
 
 ######################################################################
 # Transformer
@@ -127,28 +141,22 @@ class MultivariateFeatureSelector(SelectorMixin, BaseEstimator):
     def __init__(self, k=10):
         self.k = k
         
-    class _Parallel:
-        #helper class for calculating, in parallel, 
-        #test statistic associated with
-        #selected best features and an additional feature 
-        def __init__(self, X_new, y,best_features):
-            self.X_new = X_new
-            self.y = y
-            self.best_feat = best_features
-
-        def __call__(self, index):
-            if np.var(self.X_new[:,index]) == 0:
-                stat = -1000.0
-            else:   
-                if len(self.best_feat)==0:
-                    X_j =  self.X_new[:,index] 
-                    stat= k_sample_test(X_j,self.y)
-                else:
-                    columns = self.best_feat.copy() 
-                    columns.append(index)
-                    X_j = self.X_new[:,columns]
-                    stat= k_sample_test(X_j,self.y)
-            return stat
+    def _test_stat(self, X_new, y, best_features, index):
+        # helper function for calculating, in parallel, 
+        # test statistic associated with
+        # selected best features and an additional feature 
+        if np.var(X_new[:,index]) == 0:
+            stat = -1.0
+        else:   
+            if len(best_features)==0:
+                X_j =  X_new[:,index] 
+                stat = k_sample_test(X_j,y)
+            else:
+                columns = best_features.copy() 
+                columns.append(index)
+                X_j = X_new[:,columns]
+                stat = k_sample_test(X_j,y)
+        return stat
         
         
     def fit(self, X, y,workers = -1):
@@ -173,34 +181,40 @@ class MultivariateFeatureSelector(SelectorMixin, BaseEstimator):
         features = np.arange(X.shape[1])
         
         if np.isnan(X).any() == True:
-            raise ValueError("existing non-parametric multivariate independence tests in scipy do not allow nan")
-        if type(self.k) != int:
-            raise ValueError("k features to select must be integer")
-        if not 0 <= self.k <= X.shape[1]:
-                raise ValueError("k features to select must be a non-negative integer that is less than or equal to n_features of X")
+            raise ValueError("existing multivariate independence tests in scipy do not allow nan")
+        if type(self.k) is not int:
+            raise TypeError("k is type {}, must be int where {} is type(k)")
+        if not 0 < self.k and self.k <= X.shape[1]:
+                raise ValueError("k is {}, must be nonnegative <= number of features of X where {} is k")
         if not X.shape[0] >= 5:
-                raise ValueError("n_samples of data matrix X must be >= 5 in order to perform k_sample multivariate independence test")
+                raise ValueError("number of samples is {}, must be >= 5 where {} is X.shape[0]")
         
-        #loop to select k best features, 
-        #each iteration adds next best feature
+        # loop to select feature subset, 
+        # each iteration adds next best feature as 
+        # determined by the mulitivariate independence test
+        # as we rank each additional feature by statistic
         best_features = []
         while len(best_features) < self.k: 
             X_new = np.array(X)
             
-            #Mapwrapper parallelizes test statistic calculations 
-            #of selected best features and each additional feature.
-            #size of operations in parallel per loop iteration is 
-            #n_features - len(best_features)
-            parallel = self._Parallel(X_new=X_new, y=y,best_features = best_features)
-            with MapWrapper(workers) as mapwrapper:
-                scores = list(mapwrapper(parallel, features)) 
-            scores_index = np.zeros((len(features),2)) 
-            scores_index[:,0] = features 
-            scores_index[:,1] = scores 
-            sorted_index = scores_index[scores_index[:, 1].argsort()] 
-            best = sorted_index[len(scores)-1,0] 
-            best_features.append(int(best)) 
-            features = np.delete(features,np.where(features == best))
+            # Parallel process for test statistic calculations 
+            # of selected best features and each additional feature.
+            # size of operations in parallel per loop iteration is 
+            # n_features - len(best_features)
+            scores = list(
+                Parallel(n_jobs=workers)(
+                    [
+                        delayed(self._test_stat)(X_new,y,best_features,index)
+                        for index in features
+                    ]
+                )
+            )
+
+            scores_index = np.column_stack((features,np.array(scores)))
+            sorted_index = scores_index[scores_index[:,1].argsort()] 
+            best = int(sorted_index[len(scores)-1,0])
+            best_features.append(best) 
+            features = np.delete(features,np.where(features == best)) 
         self.best_features_ = best_features
         self.features_ = np.arange(X.shape[1])
         return self
